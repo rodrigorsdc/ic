1dff213fadd9866b0c863a6e641576a4b53d2871
Rodrigo Ribeiro <rodrigorsdc@gmail.com>
2019-02-17 21:55:12 -0300

    Add new function, to serve pml_graph

--- src/cpp/util.cpp
+++ src/cpp/util.cpp
@@ -6,13 +6,27 @@ using namespace std;
 
 extern const double INF = 1.0 / 0.0;
 
+static void check_mcalloc(void *ptr) {
+    if (ptr == NULL) {
+	printf("malloc/calloc return NULL!\n");
+	exit(EXIT_FAILURE);
+    }
+}
+
 int* malloc_int(int n) {
     int* ptr;
     ptr = (int*) malloc(n * sizeof(int));
-    if (ptr == NULL) {
-        printf("malloc devolveu NULL!\n");
-        exit(EXIT_FAILURE);
-    }
+    check_mcalloc(ptr);
     return ptr;
 }
 
+int **matrix_int(int n, int m) {
+    int **ptr;
+    ptr = (int **) calloc(n, sizeof(int *));
+    check_mcalloc(ptr);
+    for (int i = 0; i < n; i++) {
+	ptr[i] = (int *) calloc(m, sizeof(int));
+	check_mcalloc(ptr[i]);
+    }
+    return ptr;    
+}
--- src/cpp/util.h
+++ src/cpp/util.h
@@ -2,6 +2,7 @@
 #define UTIL_H
 
 int* malloc_int(int n);
+int **matrix_int(int n, int m);
 
 extern const double INF;
 
