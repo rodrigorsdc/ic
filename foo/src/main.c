dcb0ae17c9ad985e46609eadf3fed9060cf3568a
Rodrigo Ribeiro <rodrigorsdc@gmail.com>
2019-04-05 21:39:20 -0300

    Implement graph (PML_GRAPH) mode.

--- foo/src/main.c
+++ foo/src/main.c
@@ -1,8 +1,61 @@
 #include <R.h>
 #include <Rinternals.h>
-/* #include "pml_graph.h" */
+#include "pml_graph.h"
+#include "util.h"
 
-SEXP pml_graph(SEXP A_size, SEXP sample, SEXP c, SEXP k) {
+void flatten_to_matrix(int **M, int n, int m, int *V) {
+    int k = 0;
+    for (int i = 0; i < n; i++)
+	for (int j = 0; j < m; j++)
+	    M[i][j] = V[k++];
+}
+
+void matrix_to_flatten(int *V, int **M, int n, int m) {
+    int k = 0;
+    for (int i = 0; i < n; i++)
+	for (int j = 0; j < m; j++)
+	    V[k++] = M[i][j];
+}
+
+void print_matrix(int **M, int n, int m) {
+    for (int i = 0; i < n; i++) {
+	for (int j = 0; j < m; j++)
+	    printf("%d ", M[i][j]);
+	printf("\n");
+    }
+}
+
+void graph_setUp(struct pml_graph_data *data, SEXP A_size,
+		 SEXP sample, SEXP c, SEXP k) {
+    data->V = ncols(sample);
+    data->A_size = *(INTEGER(A_size));
+    data->sample_size = nrows(sample);
+    data->sample = (int **) malloc(data->sample_size * sizeof(int *));
+    for (int i = 0; i < data->sample_size; i++)
+	data->sample[i] = (int *) malloc(data->V * sizeof(int));
+    flatten_to_matrix(data->sample, data->sample_size,
+    		      data->V, INTEGER(sample));
+    data->adj = matrixINT(data->V, data->V);
+    data->max_edges = (data->V * (data->V - 1)) / 2;
+    data->A = array_arange(data->A_size);
+    data->num_graphs = 1 << data->max_edges;
+    if (length(c) == 3) {
+	data->fold = matrixINT(data->sample_size, data->V);
+	data->out_fold = matrixINT(data->sample_size, data->V);
+	data->cv_enable = 1;
+	data->c_min = REAL(c)[0];
+	data->c_max = REAL(c)[1];
+	data->c_interval = REAL(c)[2];
+	data->k = *(INTEGER(k));
+    } else {
+	data->cv_enable = 0;
+	data->c = *(REAL(c));
+    }    
+}
+
+
+/* PML GRAPH */
+SEXP pml_gr(SEXP A_size, SEXP sample, SEXP c, SEXP k) {
     SEXP ans;
     if (!isInteger(A_size) || length(A_size) != 1)
 	error("A_size argument must be a scalar integer");
@@ -13,16 +66,14 @@ SEXP pml_graph(SEXP A_size, SEXP sample, SEXP c, SEXP k) {
     if (length(c) == 3 && !isInteger(k))
 	error("k argument must be a scalar integer");
     ans = PROTECT(allocMatrix(INTSXP, ncols(sample), ncols(sample)));
-    printf("dfwe");
-    /* struct pml_graph_data *data = (struct pml_graph_data *) */
-    /* 	malloc(sizeof(struct pml_graph_data)); */
-
-    /* data->V = ncols(sample); */
-    /* data->A_size = A_size; */
-    /* data->sample_size = nrows(sample); */
-    /* /\* data->sample = REAL(sample) *\/ */
-    /* data->k = *(INTEGER(k)); */
-	
+    
+    struct pml_graph_data *data = (struct pml_graph_data *)
+    	malloc(sizeof(struct pml_graph_data));
+    graph_setUp(data, A_size, sample, c, k);
+    pml_graph(data);
+    if (length(c) == 3)
+	printf("best regularizer: %lf\n", data->c);
+    matrix_to_flatten(INTEGER(ans), data->adj, data->V, data->V);
     UNPROTECT(1);
     return ans;
 }
