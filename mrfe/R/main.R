718f235dbd74c8494807ad3dc8edc1816d9a54bc
Rodrigo Ribeiro <rodrigorsdc@gmail.com>
2019-07-18 18:22:38 -0300

    Add conservative and non-conservative function for mrfe package

--- mrfe/R/main.R
+++ mrfe/R/main.R
@@ -1,11 +1,40 @@
 "mrfe" <- function(A, sample, c, max_neigh=NULL) {
-     return (.Call('Rmrfe', A,
-                    sample, c,
-                   max_neigh))
+     return (.Call('Rmrfe', A, sample, c, max_neigh))
 }
 
 "cv.mrfe" <- function(A, sample, c_lower, c_upper, c_step, k=10,
                       max_neigh=NULL) {
     return (.Call('Rmrfe_cv', A, sample, c(c_lower, c_upper, c_step),
                                            max_neigh, k))
+}
+
+"con.mrfe" <- function(A, sample, c, max_neigh=NULL) {
+    list.adj <- .Call('Rmrfe', A, sample, c, max_neigh)
+    n <- length(list.adj)
+    matrix.adj <- matrix(rep(0, n**2), ncol=n)
+    for (i in 1:n) {
+        for (j in 1:n) {
+            if (is.element(j, list.adj[[i]]) &&
+                is.element(i, list.adj[[j]])) {
+                matrix.adj[i, j] <- 1
+                matrix.adj[j, i] <- 1
+            }
+        }
+    }
+    return (matrix.adj)
+}
+
+"ncon.mrfe" <- function(A, sample, c, max_neigh=NULL) {
+    list.adj <- .Call('Rmrfe', A, sample, c, max_neigh)
+    n <- length(list.adj)
+    matrix.adj <- matrix(rep(0, n**2), ncol=n)
+    for (i in 1:n) {
+        for (j in 1:n) {
+            if (is.element(j, list.adj[[i]])) {
+                matrix.adj[i, j] <- 1
+                matrix.adj[j, i] <- 1
+            }
+        }
+    }
+    return (matrix.adj)
 }
